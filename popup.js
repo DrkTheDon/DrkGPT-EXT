2d3cbdf7378bf2df24807b12c148f23c72768a4b
Niti <74876947+DrkTheDon@users.noreply.github.com>
2023-01-26 11:46:36 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,34 @@
+/////////////////////////////////////////////////////
+// DrK ChatGPT Writer (DrkGPT)                     //
+// We do a little bit of trolling.                 //
+// https://drkbro.ml/                              //
+// Coded by: drk, DragonSlayer64                   //
+/////////////////////////////////////////////////////
 
+const express = require("express");
+const app = express();
+const { Configuration, OpenAIApi } = require("openai");
+const prompt = require("prompt-sync")();
+
+const configuration = new Configuration({
+  apiKey: "Your Api code",
+});
+
+
+//api,post
+const openai = new OpenAIApi(configuration);
+
+const completionFunction = async () => {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: "what is the capital of germany?",
+  });
+
+  console.log(completion.data.choices[0].text);
+};
+
+
+//prompt,request
+const userQuestion = prompt("Enter a prompt: ");
+completionFunction(userQuestion);
+console.log("Generating Answer, this might take some time.")
